c55dac3094f1df48fa648aa5c70af134fd690a54
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-04-26 19:10:18 +0530

    init

--- app/card/page.tsx
+++ app/card/page.tsx
@@ -4,7 +4,7 @@ import { User } from "@/lib/types";
 export default function AuthenCityCard({ UserData }: { UserData: User }) {
   return (
     <div>
-      User Authencity Card
+      User Authencity Card is this
     </div>
   )
 }
